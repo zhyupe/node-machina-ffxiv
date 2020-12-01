d4cb1ce7c637fb4ff3157ebdd58062adf6a3f03d
zhyupe <zyp108421@gmail.com>
2020-12-01 20:47:25 +0800

    feat: operation offset for CN 5.3

--- models/inventoryModifyHandler.js
+++ models/inventoryModifyHandler.js
@@ -16,7 +16,7 @@ const operationsOffset = (region) => {
         case "Global":
             return 0x013C;
         case "CN":
-            return 0x6d;
+            return 0x012E;
         case "KR":
             return 0x039A;
     }
